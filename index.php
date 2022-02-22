b7c2a5ea267ccb403c686b0c11305354e2dd4141
tiasari <thiyashyari@gmail.com>
2022-02-22 16:09:08 +0700

    projek percobaan 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head> <title> LOGIN USER </title>
+</head>
+<body bgcolor="#dcdcdc">
+<center>
+<form action=prosesloginuser.php method="post">
+<table bgcolor="white">
+<tr> <td align=center colspan=2><marquee> SILAHKAN LOGIN DISINI !!!! </marquee><br> <img src=img/admin.jpg></td></tr>
+<tr > <td> Username&nbsp;&nbsp;:  </td> 
+<td> <input name="username" type="text" id="username" size="30" maxlength="30"></td> </tr>
+<tr > <td> Password &nbsp; : </td> 
+<td> <input name="password" type="password" id="password" size="30" maxlength="30"></td> </tr>
+<tr> <td align=center colspan=2> <input name="login" type=submit value="Login"> 
+<input name="batal" type=reset value="Cancel"> </td></tr>
+</table></form>
+</html>
